442e4cace0a03620ccf9afcefd71c64e83ddda0e
Xlxw <xlxw@xlxw.org>
2023-04-05 16:22:26 +0800

    Fix bug for #11

--- model/STG-correction/DataProcessor/GeneratorDataset.py
+++ model/STG-correction/DataProcessor/GeneratorDataset.py
@@ -58,7 +58,7 @@ class GeneratorDataset(Dataset):
 
     def _extract_gendata(self, sentences : list, labels : list) -> tuple:
         '''
-        Extract Data that Contains I / MI
+        Extract Data that Contains I / MI / M
         :param sentences: sentences collection list
         :param labels: labels list
         :return: extracted sentence, label
@@ -74,7 +74,7 @@ class GeneratorDataset(Dataset):
             if 'Insert' in label or 'Modify' in label:
                 if 'Modify' in label:
                     mod_op = label['Modify']
-                    MI_flags = [True if 'INS' in mod['tag'] else False for mod in mod_op]
+                    MI_flags = [True if 'INS' in mod['tag'] or '+' not in mod['tag'] else False for mod in mod_op]
                     if True not in MI_flags:
                         continue
                 post_sentences.append(sentence)
